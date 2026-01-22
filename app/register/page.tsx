21401761bd67e4b0428ee7d437fc707097e9ee72
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2026-01-21 21:00:43 -0300

    feat: creating register page and conect with BE

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,10 +3,12 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [nationality, setNationality] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -17,23 +19,22 @@ export default function LoginPage() {
     setError(null);
 
     try {
-      const res = await fetch('http://localhost:3333/auth/login', {
+      const res = await fetch('http://localhost:3333/users/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, name, nationality }),
       });
 
       if (!res.ok) {
         const errorData = await res.json();
         throw new Error(errorData.message || 'Login failed');
       }
-      const data = await res.json();
 
-      localStorage.setItem('token', data.token);
-      router.push('/trips');
-    } catch (error: unknown) {
+      router.push('/login');
+
+    }catch (error: unknown) {
       if (error instanceof Error) {
         setError(error.message);
       } else {
@@ -59,6 +60,27 @@ export default function LoginPage() {
         <h1 className='text-3xl font-bold mb-8 text-center text-purple-700'>MyTrip</h1>
 
         {error && <p className='text-red-500 mb-4'>{error}</p>}
+
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-700 mb-2'>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full
+              rounded-lg
+              border
+              border-gray-300
+              px-4
+              py-2.5
+              text-sm
+              focus:outline-none
+              focus:ring-2
+              focus:ring-purple-400
+              focus:border-purple-400"
+            required
+          />
+        </div>
 
         <div className='mb-4'>
           <label className='block text-sm font-medium text-gray-700 mb-2'>Email</label>
@@ -102,6 +124,32 @@ export default function LoginPage() {
               focus:border-purple-400"
             required
           />
+          <p className="flex items-start mt-2 text-xs text-slate-400">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-1.5">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+            </svg>
+      
+            Password must contain at least one letter and one number, and can include special characters
+          </p>
+        </div>
+
+        <div className='mb-4'>
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Nationality
+          </label>
+
+          <select
+            name="nationality"
+            className="w-full rounded-xl border border-gray-300 bg-white px-4 py-3 text-sm focus:border-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-200"
+            value={nationality}
+            onChange={(e) => setNationality(e.target.value)}
+          >
+            <option value="">Select your nationality</option>
+            <option value="BR">🇧🇷 Brazilian</option>
+            <option value="US">🇺🇸 American</option>
+            <option value="FR">🇫🇷 French</option>
+          </select>
+
         </div>
 
         <button
@@ -111,20 +159,10 @@ export default function LoginPage() {
           {loading ? 'Signing in...' : 'Sign In'}
         </button>
 
-        <div className="mt-4 text-center">
-          <span className="text-sm text-gray-600">
-            Already have an account?
-          </span>{' '}
-          <button
-            type="button"
-            onClick={() => router.push('/register')}
-            className="text-sm text-purple-600 hover:underline"
-          >
-            Sign in
-          </button>
-        </div>
+        
 
        </form>
     </div>
-);
+  )
+
 }
