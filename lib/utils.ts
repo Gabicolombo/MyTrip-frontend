85634198f0ae7a1c4ea41ac2286a1f5a45828f52
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2026-04-03 19:40:48 -0300

    feat: improve the formatDate

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,11 @@
 export function formatDate(dateStr: string) {
-  return new Date(dateStr).toLocaleDateString('en-GB', {
+  if (!dateStr) return '';
+  const [datePart] = dateStr.split('T');
+  const [year, month, day] = datePart.split('-');
+
+  const date = new Date(year, month - 1, day);
+
+  return date.toLocaleDateString('en-GB', {
     day: '2-digit',
     month: 'short',
     year: 'numeric',
