ff31556915c22ac3a2a6521d47454b447f68561c
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2026-05-11 16:45:07 -0300

    feat: hability to be able to delete the trip

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import RegisterTripModal from '@/components/trip/RegisterTripModal';
 import AddDestinationsModal from '@/components/trip/AddDestinationModal';
-
+import { Trash2 } from 'lucide-react';
+import ConfirmModal from '@/components/common/confirmModal';
 interface Destination {
   id: string;
   city: string;
@@ -22,6 +23,7 @@ interface Trip {
   endDate: string;
   destinations: Destination[];
   imageUrl: string;
+  status: 'Initiated' | 'Completed';
 }
 
 export default function HomePage() {
@@ -30,7 +32,30 @@ export default function HomePage() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
+  const [deletingTrip, setDeletingTrip] = useState<Trip | null>(null);
   const [destinationsTripId, setDestinationsTripId] = useState<string | null>(null);
+  const name = localStorage.getItem('name') || 'Traveler';
+
+  async function deleteTrip(tripId: string) {
+
+    try {
+      console.log('Attempting to delete trip with ID:', tripId);
+      const response = await fetch(`http://localhost:4000/trips/delete-trip/${tripId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      if (!response.ok) throw new Error('Failed to delete trip');
+      setTrips(prev => prev.filter(trip => trip.id !== tripId));
+      return `Trip ${tripId} deleted successfully`;
+    }catch (err: unknown) {
+      setError(err instanceof Error ? err.message : 'Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  }
 
   async function fetchTrips() {
     try {
@@ -43,6 +68,7 @@ export default function HomePage() {
       if (!response.ok) throw new Error('Failed to fetch trips');
 
       const data = await response.json();
+      console.log(data);
       setTrips(data);
     } catch (err: unknown) {
       setError(err instanceof Error ? err.message : 'Something went wrong');
@@ -61,7 +87,7 @@ export default function HomePage() {
 
         {/* Header */}
         <div className="flex justify-between items-center mb-10">
-          <h1 className="text-3xl text-purple-600 font-bold">Welcome back 👋</h1>
+          <h1 className="text-3xl text-purple-600 font-bold">Welcome back {name} 👋</h1>
           <button
             onClick={() => setIsModalOpen(true)}
             className="px-5 py-2 rounded-lg bg-purple-600 text-white hover:bg-purple-700">
@@ -87,22 +113,40 @@ export default function HomePage() {
         {trips.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {trips.map((trip) => {
-              const isCompleted = new Date(trip.endDate).getTime() < new Date().getTime();
+              const isCompleted = trip.status === 'Completed';
               const hasDestinations = trip.destinations.length > 0;
-            
+             
               return (
                 <div
                   key={trip.id}
-                  onClick={() => router.push(`/trip/${trip.id}`)}
                   className={`p-6 rounded-2xl shadow-sm hover:shadow-lg transition cursor-pointer ${
                     isCompleted
                       ? 'bg-red-50 border border-red-200 pointer-events-none'
                       : 'bg-green-50 border border-green-200'
                   }`}
                 >
+                  <button className='ml-auto block mb-2' onClick={() => { setConfirmModalOpen(true); setDeletingTrip(trip); }}>
+                    <Trash2 className="text-red-400 hover:text-red-600 transition-colors" size={16} />
+                  </button>
                   <img src={trip.imageUrl} alt={trip.title} />
 
-                  <h2 className="text-xl font-semibold mb-2 text-purple-600 text-center">
+                  {deletingTrip === trip && (
+                    <ConfirmModal
+                      title="Confirm Deletion"
+                      message="Are you sure you want to delete this trip? This action cannot be undone."
+                  
+                      onConfirm={() => {
+                        deleteTrip(trip.id);
+                        setConfirmModalOpen(false);
+                        setDeletingTrip(null);
+                      }}
+                      onCancel={() => {
+                        setConfirmModalOpen(false); 
+                      }}
+                    />
+                  )}
+
+                  <h2 className="text-xl font-semibold m-2 text-purple-600 text-center">
                     {trip.title}
                   </h2>
 
@@ -139,6 +183,11 @@ export default function HomePage() {
                       <span className="text-green-600 font-semibold">Upcoming</span>
                     )}
                   </p>
+                  <button 
+                    onClick={() => router.push(`/trip/${trip.id}`)}
+                    className='px-4 py-2 mt-4 bg-purple-600 text-white rounded-lg hover:bg-purple-700 block mx-auto'>
+                    Details
+                  </button>
                 </div>
               );
             })}
